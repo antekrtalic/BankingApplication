734c20a2b5feb23c2c922dcb00da8bd73c49df1b
Ante Dragoje <antekrtalic2@gmail.com>
2021-11-03 18:21:59 +0100

    GUI for card User set

--- src/com/example/bankingapplication/BankingApplication/BankAccount.java
+++ src/com/example/bankingapplication/BankingApplication/BankAccount.java
@@ -15,6 +15,10 @@ public class BankAccount {
         this.customerName = customerName;
     }
 
+    public int getBalance() {
+        return this.balance;
+    }
+
     public String getCustomerID() {
         return this.customerID;
     }
@@ -51,8 +55,46 @@ public class BankAccount {
 
     public void showMenu() {
         char option = '\0';
+        int amount = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome " + this.getCustomerName() + "!");
-        System.out.println("Your ");
+        System.out.println("Your ID is " + this.getCustomerID() + "!");
+        System.out.println("\n");
+        System.out.println("What do you want to do?");
+        System.out.println("A. Check Balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous transaction");
+        System.out.println("E. Exit");
+
+        do {
+            System.out.println("=============================================================");
+            System.out.println("Enter an option:");
+            System.out.println("=============================================================");
+            option = scanner.next().charAt(0);
+
+            switch(option) {
+                case 'A':
+                    System.out.println("Balance: " + this.getBalance());
+                    break;
+                case 'B':
+                    System.out.println("Enter amount to add:");
+                    amount = scanner.nextInt();
+                    this.deposit(amount);
+                    break;
+                case 'C':
+                    System.out.println("Enter amount to withdraw:");
+                    amount = scanner.nextInt();
+                    this.withdraw(amount);
+                    break;
+                case 'D':
+                    this.cardTransactionMessage();
+                    break;
+                default:
+                    System.out.println("Invalid option! Please use command from list!");
+                    break;
+            }
+
+        } while(option != 'E');
     }
 }
